e7eccad29d6a988caf9e8aa9b0a6b5a390f417ef
r0b0t <antoniomainakn@gmail.com>
2018-06-10 11:41:10 +0300

    Added jquery. Created the side-nav. Made the landing and about components responsive.

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    var mobile_nav = document.querySelector('#mobile-nav');
+    mobile_nav.style.width = '350px';
+}
+
+function closeNav() {
+    var mobile_nav = document.querySelector('#mobile-nav');
+    mobile_nav.style.width = '0';
+}
